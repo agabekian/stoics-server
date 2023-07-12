80a4be7111731c398b63ab44f2e04af1dd4f4554
Armen Agabekian <2008nv@gmail.com>
2023-07-11 23:12:22 -0700

    mmm

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -6,5 +6,5 @@ mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-    .then(() => console.log("Established connection so a local ? ", process.env.DB_URL))
+    .then(() => console.log("It says - established connection to: ", process.env.DB_URL))
     .catch(err => console.log("Something went wrong while connecting", err))
